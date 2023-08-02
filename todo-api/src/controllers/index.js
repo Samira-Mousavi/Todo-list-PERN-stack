9a18ecfe31e60b7c584905183d5c7bdfef368a7e
Samira <smoosavi2001@yahoo.com>
2023-08-02 21:41:05 +0200

    controller module

---
+++ todo-api/src/controllers/index.js
@@ -0,0 +1,11 @@
+const createTodo = require('./createTodo')
+const getTodos = require('./getTodos')
+const deleteTodo = require('./deleteTodo')
+const updateTodo = require('./updateTodo')
+
+module.exports = {
+	getTodos,
+	createTodo,
+	updateTodo,
+	deleteTodo
+}
