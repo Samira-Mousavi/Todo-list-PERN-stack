a809da97c575a95deabd5888a1f8f10a5e64eb2d
Samira <smoosavi2001@yahoo.com>
2023-08-02 21:42:06 +0200

    sequeilize

---
+++ todo-api/src/migrations/20211225065520-create-todo.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      parentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Todos',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Todos');
+  }
+};
