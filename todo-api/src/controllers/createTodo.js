79efa6b14b598ae002df09b9618204c9b429d873
Samira <smoosavi2001@yahoo.com>
2023-08-06 13:10:03 +0200

    commit edition

--- todo-api/src/controllers/createTodo.js
+++ todo-api/src/controllers/createTodo.js
@@ -5,12 +5,13 @@ module.exports = (req, res) => {
 		title: req.body.title,
 		parentId: req.body.todo_id ? req.body.todo_id : null
 	})
-		// it responds back to the client with the result of these operations in the form of a JSON response
+
 		.then(todo => {
 			if (req.body.todo_id) {
 				return Todos.update(
 					{ status: false },
 					{ where: { id: req.body.todo_id } }
+					// it responds back to the client with the result of these operations in the form of a JSON response
 				).then(updatedTodo => res.status(201).send({ success: true, data: { todo } }))
 					.catch(err => res.status(400).send(err))
 			} else {
