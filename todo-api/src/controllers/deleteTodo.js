93ee2904c16b93d392f6297820904fb316ee906a
Samira <smoosavi2001@yahoo.com>
2023-08-06 13:10:32 +0200

    response part

--- todo-api/src/controllers/deleteTodo.js
+++ todo-api/src/controllers/deleteTodo.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
 		where: {
 			id: req.query.id
 		}
-	})
+	})// it responds back to the client with the result of these operations in the form of a JSON response
 		.then(todo => res.status(201).send({ success: true, data: { todo } }))
 		.catch(err => res.status(400).send(err))
 }
